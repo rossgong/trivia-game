eefd009eda871abfcfad0536d014bc46d5edb9d3
Ross Gongaware <rossgong@gmail.com>
2018-11-15 18:36:59 -0500

    Added a timer and a little more style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,17 +6,28 @@ var game = {
 	corrects: 0,
 	wrongs: 0,
 	currentTimeout: 0,
+	timeLeft: 10,
 
 	questions: [
-		{question: "What is 2+1?", answers: ["1","2","3","4"], answerIndex: 2},
-		{question: "What is 1+3?", answers: ["1","2","3","4"], answerIndex: 3},
-		{question: "What is 1+1?", answers: ["1","2","3","4"], answerIndex: 1},
-		{question: "What is 2+2?", answers: ["1","2","3","4"], answerIndex: 3},
+		{
+			question: "Which country has the largest landarea?",
+			answers: ["United States of America", "Canada", "China", "India"], answerIndex: 1
+		},
+		{
+			question: "Which country has the largest population",
+			answers: ["Nigeria", "Russia", "United States of America", "India"], answerIndex: 3
+		},
+		{
+			question: "Which country is the most northern?",
+			answers: ["Croatia", "Albania", "Macedonia", "Greece"], answerIndex: 0
+		},
+		{
+			question: "Which country is the most southern?",
+			answers: ["Croatia", "Albania", "Greece", "Macedonia"], answerIndex: 2
+		},
 	],
 
-
-
-	intializeGame: function() {
+	intializeGame: function () {
 		this.currentQuestionIndex = 0;
 		this.corrects = 0;
 		this.wrongs = 0;
@@ -24,28 +35,52 @@ var game = {
 		this.askQuestion(this.currentQuestionIndex);
 	},
 
-	askQuestion: function(questionIndex) {
+	askQuestion: function (questionIndex) {
 
 		console.log(this);
 		$("#trivia-question").text(this.questions[questionIndex].question);
+		$()
 
 		//clear out answers container of whatever answers were there previously and store it to add answers in forreach loop
 		var answerContainer = $("#trivia-answers-container").html("");
-		for (var i = 0; i < this.questions[questionIndex].answers.length; i ++) {
-			answerContainer.append(this.constructAnswerDiv(this.questions[questionIndex].answers[i], i));
+		var answers = [];
+
+		for (var i = 0; i < this.questions[questionIndex].answers.length; i++) {
+			answers.push(this.constructAnswerDiv(this.questions[questionIndex].answers[i], i));
 		}
 
-		this.currentTimeout = setTimeout(function() {
-			game.answerQuestion(-1);
-		}, 3000);
+		while (answers.length > 0) {
+			var randomAnswer = Math.floor(Math.random() * answers.length);
+			answerContainer.append(answers[randomAnswer]);
+			answers.splice(randomAnswer, 1);
+		}
+
+		this.timeLeft = 10;
+		this.timeRemainingUpdate(this.timeLeft);
+
+		this.timeoutFunction();
 	},
 
-	constructAnswerDiv: function(answer, index) {
-		var answerDiv = $("<div>").attr("class", "trivia-answer").attr("value", index).text(answer)
+	timeoutFunction: function () {
+		if (game.timeLeft > 0) {
+			game.timeLeft--;
+			game.currentTimeout = setTimeout(game.timeoutFunction, 1000);
+		} else {
+			game.answerQuestion(-1);
+		}
+		game.timeRemainingUpdate(game.timeLeft) + 1;
+	},
+
+	timeRemainingUpdate: function (secs) {
+		$("#time-remaining").html("There are <strong>" + secs + "</strong> seconds remaining!");
+	},
+
+	constructAnswerDiv: function (answer, index) {
+		var answerDiv = $("<div>").attr("class", "trivia-answer").attr("data-index", index).text(answer)
 
 		//TODO: add listener for click on answerDiv
-		answerDiv.on("click", function(event) {
-			game.answerQuestion(parseInt(this.getAttribute("value")));
+		answerDiv.on("click", function (event) {
+			game.answerQuestion(parseInt(this.getAttribute("data-index")));
 		});
 
 		return answerDiv
@@ -57,18 +92,25 @@ var game = {
 		//if correct
 		if (answerIndex === this.questions[this.currentQuestionIndex].answerIndex) {
 			this.corrects++;
+			$("#trivia-question").html("Wowwee good job!<br>Get ready for another!");
 		} else {
 			this.wrongs++;
+			if (answerIndex === -1) {
+				$("#trivia-question").html("Outta Time!<br>Get ready for another!");
+			} else {
+				$("#trivia-question").html("WRONG<br>Get ready for another!");
+			}
 		}
-
-		console.log("w:" + this.corrects + "||l:" + this.wrongs);
 
 		this.nextQuestion();
 	},
 
-	nextQuestion: function() {
+	nextQuestion: function () {
 		this.currentQuestionIndex++;
 		clearTimeout(this.currentTimeout);
+		console.log("here");
+		console.log($("#time-remaining").html(""));
+		console.log("here2");
 
 		if (this.currentQuestionIndex < this.questions.length) {
 			setTimeout(function () {
@@ -79,30 +121,29 @@ var game = {
 		} else {
 			this.showResults();
 		}
-		
+
 	},
 
-	showCurrentStats: function() {
-		$("#trivia-question").html("CURRENT STATS");
+	showCurrentStats: function () {
 		$("#trivia-answers-container").html("Correct: " + this.corrects + "<br>" +
-		"Wrong: " + this.wrongs);
+			"Wrong: " + this.wrongs);
 	},
 
 	showResults: function () {
+		$("#trivia-question").html("WOW that was fast<br>Let's see how ya did!");
 		this.showCurrentStats();
 
 		//adds a reset button
-		$("<button>").text("Reset").appendTo("#trivia-container").on("click", function(event) {
+		$("<button>").text("Reset").appendTo("#trivia-container").on("click", function (event) {
 			this.remove();
 			game.intializeGame();
 		});
 	},
 }
 
-$(document).ready( function() {
-	$("#start-button").on("click", function(event) {
+$(document).ready(function () {
+	$("#start-button").on("click", function (event) {
 		this.remove();
 		game.intializeGame();
 	});
-	
 });
